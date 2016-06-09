ea7b3e50781447886be2bf94e228fa0293b8b18d
Vinay Jambhale <VPPL-10132@Vakil-Server.com>
2016-06-09 18:45:23 +0530

    Initial commit

---
+++ app/src/main/java/app/num/bargraphmp/MainActivity.java
@@ -0,0 +1,95 @@
+package app.num.bargraphmp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+//        BarChart barChart = (BarChart) findViewById(R.id.chart);
+
+		HorizontalBarChart barChart = (HorizontalBarChart) findViewById(R.id.chart);
+
+		ArrayList<BarEntry> entries = new ArrayList<>();
+		entries.add(new BarEntry(4f, 0));
+		entries.add(new BarEntry(8f, 1));
+		entries.add(new BarEntry(6f, 2));
+		entries.add(new BarEntry(12f, 3));
+		entries.add(new BarEntry(18f, 4));
+		entries.add(new BarEntry(1f, 5));
+		entries.add(new BarEntry(3f, 6));
+		entries.add(new BarEntry(6f, 7));
+		entries.add(new BarEntry(19f, 8));
+		entries.add(new BarEntry(3f, 9));
+		entries.add(new BarEntry(1f, 10));
+		entries.add(new BarEntry(2f, 11));
+
+		BarDataSet dataset = new BarDataSet(entries, "# of Calls");
+
+		ArrayList<String> labels = new ArrayList<String>();
+		labels.add("January");
+		labels.add("February");
+		labels.add("March");
+		labels.add("April");
+		labels.add("May");
+		labels.add("June");
+		labels.add("july");
+		labels.add("August");
+		labels.add("September");
+		labels.add("Octember");
+		labels.add("November");
+		labels.add("December");
+
+        /* for create Grouped Bar chart
+        ArrayList<BarEntry> group1 = new ArrayList<>();
+        group1.add(new BarEntry(4f, 0));
+        group1.add(new BarEntry(8f, 1));
+        group1.add(new BarEntry(6f, 2));
+        group1.add(new BarEntry(12f, 3));
+        group1.add(new BarEntry(18f, 4));
+        group1.add(new BarEntry(9f, 5));
+
+        ArrayList<BarEntry> group2 = new ArrayList<>();
+        group2.add(new BarEntry(6f, 0));
+        group2.add(new BarEntry(7f, 1));
+        group2.add(new BarEntry(8f, 2));
+        group2.add(new BarEntry(12f, 3));
+        group2.add(new BarEntry(15f, 4));
+        group2.add(new BarEntry(10f, 5));
+
+        BarDataSet barDataSet1 = new BarDataSet(group1, "Group 1");
+        //barDataSet1.setColor(Color.rgb(0, 155, 0));
+        barDataSet1.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        BarDataSet barDataSet2 = new BarDataSet(group2, "Group 2");
+        barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        ArrayList<BarDataSet> dataset = new ArrayList<>();
+        dataset.add(barDataSet1);
+        dataset.add(barDataSet2);
+        */
+
+		BarData data = new BarData(labels, dataset);
+		// dataset.setColors(ColorTemplate.COLORFUL_COLORS); //
+		barChart.setData(data);
+		barChart.animateY(1000);
+
+	}
+}
